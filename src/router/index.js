11ab045cc2d84c5cc7560187cc0cd87edba47169
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2024-05-26 10:21:36 +0200

    has history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import MainPage from '@page/MainPage.vue';
 import Projets from '@page/Projets.vue';
@@ -45,7 +45,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
