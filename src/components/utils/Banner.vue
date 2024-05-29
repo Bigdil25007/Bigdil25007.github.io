1523c4e9c87c80969a6aaf3ae3f839ff8383449d
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2024-05-29 07:44:54 +0200

    fix blue filter on text

--- src/components/utils/Banner.vue
+++ src/components/utils/Banner.vue
@@ -17,7 +17,9 @@ const props = defineProps({
 
 <template>
     <div class="banner" :style="{ height: props.height }">
-        <img :src="props.src" :alt="props.alt">
+        <div class="img-wrapper">
+            <img :src="props.src" :alt="props.alt" :style="{ height: props.height }">
+        </div>
         <div class="slot-wrapper">
           <slot></slot>
         </div>
@@ -29,15 +31,9 @@ const props = defineProps({
   position: relative;
   width: 100vw;
 
-  & img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    filter: brightness(60%);
-  }
-
-  &::after {
+  & .img-wrapper::after {
     content: "";
+    object-fit: cover;
     position: absolute;
     top: 0;
     left: 0;
@@ -46,15 +42,26 @@ const props = defineProps({
     background: rgba(31, 31, 90, 0.219); /* blue overlay */
     filter: blur(8px);
   }
+
+  & img {
+    width: 100%;
+    object-fit: cover;
+    filter: brightness(60%);
+  }
 }
 
 .slot-wrapper {
   position: absolute;
   top: 50%;
   left: 50%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+
   transform: translate(-50%, -50%);
   color: white;
-  text-align: center;
   
   & * {
     font-size: 1.5rem;
